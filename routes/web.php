8ed79b261fa25bcef702f3d74efd5b4f744b4f6c
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 14:07:14 +0900

    routesを修正

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,10 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->middleware('auth');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', [PageController::class, 'create'])->name('pages.create');
+Route::get('/', [PageController::class, 'create'])->name('pages.create')->middleware('auth');
 Route::post('/pages', [PageController::class, 'store'])->name('pages.store');
 Route::get('/pages/{page}', [PageController::class, 'show'])->name('pages.show');
