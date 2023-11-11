0bfb75484979a5a476905dec60a1fff6f9042023
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 19:20:18 +0900

    #code-containerの中身を編集可能に変更。<div>タグを<textarea>タグに置き換え、idをcode-containerからcode-editorに変更。

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -6,15 +6,18 @@
         {!! $page->content->style_css !!}
 
         /* code-containerに適用されるスタイル */
-        #code-container {
+        #code-editor {
             background-color: #1e1e1e; /* VSCodeの背景色 */
             color: white; /* 文字色 */
             font-family: Consolas, 'Courier New', monospace; /* フォント */
-            white-space: pre; /* ソースコードのフォーマットを維持 */
             padding: 10px;
             margin: 10px 0;
             border-radius: 4px;
             overflow: auto;
+            width: 100%; /* 幅を調整 */
+            height: 300px; /* 高さを調整 */
+            border: none; /* ボーダーを除去 */
+            resize: none; /* サイズ変更を不可に */
         }
         .copy-button {
             background-color: #007acc;
@@ -28,10 +31,10 @@
 </head>
 <body>
 
-    <!-- ソースコード表示エリア -->
-    <div id="code-container">
+    <!-- 編集可能なコードエリア -->
+    <textarea id="code-editor">
         {!! htmlspecialchars($page->content->body_html) !!}
-    </div>
+    </textarea>
     <!-- コピー用ボタン -->
     <button class="copy-button" onclick="copyCode()">コピー</button>
 
@@ -41,7 +44,7 @@
 
         // 追加されたJavaScript
         function copyCode() {
-            var code = document.getElementById('code-container').innerText;
+            var code = document.getElementById('code-editor').value;
             navigator.clipboard.writeText(code).then(function() {
                 alert('コードをコピーしました！');
             }, function(err) {
