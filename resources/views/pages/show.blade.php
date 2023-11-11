6f1d7c69e7e538b00bfa0755991f6acca10491b9
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 20:12:50 +0900

    コードエディタを横に3つ並べる

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -9,9 +9,19 @@
         /* code-containerに適用されるスタイル */
         #code-editor-container {
             position: relative;
-            width: 90%;
-            margin: 10px 0;
+            width: 100%;
+            /* 変更：幅を拡大 */
+            margin: 10px auto;
+            /* 中央揃え */
             padding-top: 20px;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .code-editor-wrapper {
+            width: 32%;
+            /* 変更：各エディタの幅を拡大 */
+            /* マージンやパディングを必要に応じて調整 */
         }
 
         #code-editor {
@@ -21,7 +31,8 @@
             padding: 10px;
             border-radius: 4px;
             overflow: auto;
-            width: 90%;
+            width: 100%;
+            /* エディタの幅をラッパーに合わせる */
             height: 100px;
             border: none;
             resize: none;
@@ -30,7 +41,6 @@
         .code-editor-label {
             position: absolute;
             top: -20px;
-            top: 0px;
             left: 0px;
             background-color: grey;
             color: white;
@@ -43,10 +53,24 @@
 
 <body>
     <div id="code-editor-container">
-        <span class="code-editor-label">HTML</span> <!-- 追加：ラベル要素 -->
-        <textarea id="code-editor">
-            {!! htmlspecialchars($page->content->body_html) !!}
-        </textarea>
+        <div class="code-editor-wrapper">
+            <span class="code-editor-label">HTML</span>
+            <textarea id="code-editor">
+                {!! htmlspecialchars($page->content->body_html) !!}
+            </textarea>
+        </div>
+        <div class="code-editor-wrapper">
+            <span class="code-editor-label">CSS</span>
+            <textarea id="code-editor">
+                /* CSSコード */
+            </textarea>
+        </div>
+        <div class="code-editor-wrapper">
+            <span class="code-editor-label">JavaScript</span>
+            <textarea id="code-editor">
+                /* JavaScriptコード */
+            </textarea>
+        </div>
     </div>
 
     {!! $page->content->body_html !!}
