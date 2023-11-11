bd75ca3c0612af46dde301e0ce7dca248a697d56
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 20:01:57 +0900

    エディタ内にHTMLのラベルを表示

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -7,25 +7,38 @@
         {!! $page->content->style_css !!}
 
         /* code-containerに適用されるスタイル */
+        #code-editor-container {
+            position: relative;
+            /* 追加：ラベルを正確に配置するための相対位置 */
+            width: 90%;
+            margin: 10px 0;
+        }
+
         #code-editor {
             background-color: #1e1e1e;
-            /* VSCodeの背景色 */
             color: white;
-            /* 文字色 */
             font-family: Consolas, 'Courier New', monospace;
-            /* フォント */
             padding: 10px;
-            margin: 10px 0;
             border-radius: 4px;
             overflow: auto;
             width: 90%;
-            /* 幅を調整 */
             height: 100px;
-            /* 高さを調整 */
             border: none;
-            /* ボーダーを除去 */
             resize: none;
-            /* サイズ変更を不可に */
+        }
+
+        .code-editor-label {
+            position: absolute;
+            /* 追加：絶対位置でラベルを配置 */
+            top: 0px;
+            /* 追加：ラベルの位置調整 */
+            left: 0px;
+            /* 追加：ラベルの位置調整 */
+            background-color: #007acc;
+            color: white;
+            padding: 2px 5px;
+            border-radius: 4px;
+            font-size: 0.8em;
         }
 
         .copy-button {
@@ -40,9 +53,12 @@
 </head>
 
 <body>
-    <textarea id="code-editor">
-        {!! htmlspecialchars($page->content->body_html) !!}
-    </textarea>
+    <div id="code-editor-container">
+        <span class="code-editor-label">HTML</span> <!-- 追加：ラベル要素 -->
+        <textarea id="code-editor">
+            {!! htmlspecialchars($page->content->body_html) !!}
+        </textarea>
+    </div>
 
     {!! $page->content->body_html !!}
 
