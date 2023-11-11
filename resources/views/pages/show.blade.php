11a7508cbf10739780643c4cad06a324995f08d3
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 22:59:47 +0900

    ソースコードをPrettierのフォーマット機能を使用して整形

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -62,10 +62,16 @@
 
     <!-- highlight.jsのコアライブラリと必要な言語、テーマ -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/xml.min.js"></script> <!-- HTML用 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/css.min.js"></script> <!-- CSS用 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js"></script> <!-- JS用 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/xml.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/css.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/javascript.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs.min.css">
+
+    <!-- Prettierのライブラリとパーサー -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/standalone.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-html.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-postcss.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-babel.min.js"></script>
 </head>
 
 <body>
@@ -95,13 +101,48 @@
     <script>
         {!! $page->content->script_js !!}
 
-        // highlight.jsの初期化
-        document.addEventListener('DOMContentLoaded', (event) => {
-            document.querySelectorAll('pre code').forEach((block) => {
-                hljs.highlightBlock(block);
+        // Prettierでフォーマットする関数
+        function formatCode() {
+            // HTML、CSS、JSのコードエディタを取得
+            const htmlCode = document.getElementById('html-editor').textContent;
+            const cssCode = document.getElementById('css-editor').textContent;
+            const jsCode = document.getElementById('js-editor').textContent;
+
+            // Prettierを使ってフォーマット
+            const formattedHtml = prettier.format(htmlCode, {
+                parser: "html",
+                plugins: prettierPlugins
             });
-        });
+            const formattedCss = prettier.format(cssCode, {
+                parser: "css",
+                plugins: prettierPlugins
+            });
+            const formattedJs = prettier.format(jsCode, {
+                parser: "babel",
+                plugins: prettierPlugins
+            });
+
+            // フォーマットされたコードをエディタに戻す
+            document.getElementById('html-editor').textContent = formattedHtml;
+            document.getElementById('css-editor').textContent = formattedCss;
+            document.getElementById('js-editor').textContent = formattedJs;
+        }
+
+        // コードをフォーマットし、highlight.jsでハイライトする関数
+        function formatAndHighlightCode() {
+            // コードをフォーマット
+            formatCode();
+
+            // highlight.jsを使用してコードをハイライト
+            document.querySelectorAll('pre code').forEach((block) => {
+                hljs.highlightElement(block);
+            });
+        }
+
+        // ページロード時にフォーマットとハイライトを実行
+        window.onload = formatAndHighlightCode;
     </script>
+
 </body>
 
 </html>
