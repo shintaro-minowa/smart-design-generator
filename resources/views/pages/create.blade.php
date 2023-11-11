3c08040e781415784a6978bd7ca935ba5a89275f
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 23:44:21 +0900

    HTMLを日本語に変換

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Smart Design Generator</title>
+    <title>スマートデザインジェネレーター</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -49,35 +49,34 @@
 
 <body>
     <div class="container">
-        <h1>Smart Design Generator</h1>
+        <h1>スマートデザインジェネレーター</h1>
         <form action="{{ route('pages.store') }}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" id="title" name="title" value="MyWebsite">
+                <label for="title">タイトル</label>
+                <input type="text" id="title" name="title" value="My Website">
             </div>
             <div class="form-group">
-                <label for="site-type">Site type</label>
+                <label for="site-type">サイトタイプ</label>
                 <select id="site-type" name="site-type">
-                    <option value="Web Application">Web Application</option>
-                    <option value="business">Business</option>
-                    <option value="blog">Blog</option>
-                    <option value="portfolio">Portfolio</option>
-                    <option value="e-commerce">E-commerce</option>
-                    <option value="homepage">Homepage</option>
-                    <option value="game">Game</option>
+                    <option value="Web Application">Webアプリケーション</option>
+                    <option value="business">ビジネス</option>
+                    <option value="blog">ブログ</option>
+                    <option value="portfolio">ポートフォリオ</option>
+                    <option value="e-commerce">Eコマース</option>
+                    <option value="homepage">ホームページ</option>
+                    <option value="game">ゲーム</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="color">Color</label>
-                <input type="text" id="color" name="color" value="blue">
+                <label for="color">色</label>
+                <input type="text" id="color" name="color" value="青">
             </div>
             <div class="form-group">
-                <label for="design-details">Design details</label>
-                <textarea id="design-details" name="design-details" rows="5"
-                    placeholder="Enter any specific design requirements here..."></textarea>
+                <label for="design-details">デザイン詳細</label>
+                <textarea id="design-details" name="design-details" rows="5" placeholder="ここに具体的なデザイン要件を入力してください。"></textarea>
             </div>
-            <button type="submit">Generate Design</button>
+            <button type="submit">デザイン生成</button>
         </form>
     </div>
 </body>
