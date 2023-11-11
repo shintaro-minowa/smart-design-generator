a8eaf593c70ff349333fc6e46b58592b70a71b4c
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-11 21:18:27 +0900

    コードエディタをフォーマットする処理を追加

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -45,6 +45,10 @@
             font-size: 0.8em;
         }
     </style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/standalone.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-html.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-postcss.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettier/2.0.3/parser-babel.min.js"></script>
 </head>
 
 <body>
@@ -73,6 +77,35 @@
 
     <script>
         {!! $page->content->script_js !!}
+
+        function formatCode() {
+            // HTML、CSS、JSのエディターからコードを取得
+            var htmlCode = document.getElementById('html-editor').value;
+            var cssCode = document.getElementById('css-editor').value;
+            var jsCode = document.getElementById('js-editor').value;
+
+            // Prettierを使用してフォーマット
+            var formattedHtml = prettier.format(htmlCode, {
+                parser: "html",
+                plugins: prettierPlugins
+            });
+            var formattedCss = prettier.format(cssCode, {
+                parser: "css",
+                plugins: prettierPlugins
+            });
+            var formattedJs = prettier.format(jsCode, {
+                parser: "babel",
+                plugins: prettierPlugins
+            });
+
+            // フォーマットしたコードをエディターに戻す
+            document.getElementById('html-editor').value = formattedHtml;
+            document.getElementById('css-editor').value = formattedCss;
+            document.getElementById('js-editor').value = formattedJs;
+        }
+
+        // ページの読み込みが完了したら、フォーマット関数を呼び出す
+        window.onload = formatCode;
     </script>
 </body>
 
