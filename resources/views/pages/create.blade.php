d7f00d79e9fcc11a9e81989a894d4bac1e644975
shintaro-minowa <shintaro-minowa@users.noreply.github.com>
2023-11-12 12:41:34 +0900

    自動フォーマット

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -9,7 +9,8 @@
     <meta property="og:url" content="https://smart-design-generator-cfa7c4008e3e.herokuapp.com/">
     <meta property="og:image" content="https://imgur.com/PGgm9Pa" />
     <title>スマートデザインジェネレーター</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <style>
         body {
